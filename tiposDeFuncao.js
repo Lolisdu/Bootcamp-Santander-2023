ca477717ecc4b8496bb16b33cd0ca8a84bddf617
Débora Oliveira <101465084+Lolisdu@users.noreply.github.com>
2023-08-28 12:55:16 -0300

    Add files via upload

---
+++ tiposDeFuncao.js
@@ -0,0 +1,19 @@
+//tipos de função
+//declarativas
+/*function funcao() {
+    console.log('Javascript é bacana')
+}
+funcao();
+
+//com nomeação
+var funcao = function() {
+    console.log('Sou uma mensagem de função de expressão!')
+}
+funcao();
+
+// arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+funcao();*/
+
