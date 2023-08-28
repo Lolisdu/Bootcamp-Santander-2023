0491640e5c75eec841270dc511a8de3c4d76b05f
Débora Oliveira <101465084+Lolisdu@users.noreply.github.com>
2023-08-28 12:53:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,9 @@
-var jogador1 = 0;
-var jogador2 = 1
-var placar;
-
-//if ternário
-jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são válidos') : 
-console.log('Jogadores Inválidos');
-
-//usando if
-    if( jogador1 > 0 && jogador2 == 0) {
-        console.log('Jogador 1 marcou ponto!');
-        placar = jogador1 > jogador2;
-//usando else if        
-    }else if (jogador2 > 0 && jogador1 == 0){
-        console.log('Jogador 2 marcou ponto');
-        placar = jogador2 > jogador1;
-//usando else        
-    }else{
-        console.log('Ninguém marcou ponto');
-    }
-
-    switch (placar) {
-        case placar = jogador1 > jogador2:
-            console.log('Jogador 1 Ganhou!');
-        break;
-        case placar = jogador2 > jogador1:
-            console.log('Jogador 2 Ganhou!');
-        break;
-        default:
-            console.log('Ninguém Ganhou')
-    }
-
-/*let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5',];
-let object = { propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3',}
-//for executa uma instrução até que ela seja falsa
-/*for(let indice = 0; indice < array.length; indice++){
-    console.log(indice);
+/*function funcao() {
+    console.log('Tudo certo com Javascript')
 }
-//for/in executa repetição a partir de uma propriedade
-for(let i in array){
-    console.log(i);
-}
-//com object
-for(i in object){
-    console.log(i)
-}
+funcao();*/
 
-//for/of - executa repetição a partir do valor
-for(i of array){
-    console.log(i);
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
 }
-
-for(i of object.propriedade1){
-    console.log(i);
-}
-
-
-var a = 0;
-while(a < 10) {
-    a++;
-    console.log(a);
-}
-
-do {
-    a++;
-    console.log(a);
-}while (a > 10)*/
+mensagem('Tudo certo', 'com Javascript');
